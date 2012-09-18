0f239c6de160ea7ccbde39bb9be94c0f2e56c886
adamwong246 <adam@spaceport.io>
2012-09-17 17:50:13 -0700

    added seeds. admin link. ability.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.delete_all
+User.create!({:email => "adamwong246@gmail.com", :roles => ["admin"], :password => "password", :password_confirmation => "password" })
+User.create!({:email => "guest@gmail.com", :roles => ["guest"], :password => "password", :password_confirmation => "password" })
+Post.create!({:title => 'lorem', :slug => 'lorem', :body => 'lorem'})
