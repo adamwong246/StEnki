6ba82f7fd38cd432aac15d4ac16384193311a34a
adamwong246 <adam@spaceport.io>
2012-09-19 11:16:17 -0700

    asset pipeline fix, maybe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Enki::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.prefix = '/assets'
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
