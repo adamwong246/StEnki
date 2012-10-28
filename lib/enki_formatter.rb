b5b26a8d9f46e790c391836eed9c038e51c5249e
adamwong246 <adam@spaceport.io>
2012-10-28 10:47:39 -0700

    better syntaxhighlighting

--- lib/enki_formatter.rb
+++ lib/enki_formatter.rb
@@ -4,7 +4,11 @@ class EnkiFormatter
       Lesstile.format_as_xhtml(
         text,
         :text_formatter => lambda {|text| RedCloth.new(CGI::unescapeHTML(text)).to_html},
-        :code_formatter => Lesstile::CodeRayFormatter
+        # :code_formatter => Lesstile::CodeRayFormatter
+        :code_formatter => lambda {|code, lang| 
+          CodeRay.scan(CGI::unescapeHTML(code), lang).div(:line_numbers => :table, :css => :class)
+        }
+        
       )
     end
   end
