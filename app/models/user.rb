fe69049ed673e702ffbc42ed6596830a4d048837
adamwong246 <adam@spaceport.io>
2012-12-15 14:32:39 -0800

    thank you jeebus, progress.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :fullname, :shortbio, :weburl
 
   def admin?
-    self.email == "zephram.cochrane.ncc.170!@gmail.com"
-  end
- 
-  def guest?
-    self.email == "guest@gmail.com"
+    self.email == "adamwong246@gmail.com@gmail.com" || self.services.any?{|e| e.uid == "http://adamwong246.myopenid.com/"}
   end
 
 end
