5a60871ebdf22f281143e220a7a3b0513568ab82
adamwong246 <adam@spaceport.io>
2012-12-15 14:06:39 -0800

    weeks! sort of

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ActiveRecord::Base
-  devise :omniauthable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable
+  # devise :omniauthable, :rememberable, :trackable, :lockable
 
   has_many :services, :dependent => :destroy
   
   attr_accessible :email, :password, :password_confirmation, :remember_me, :fullname, :shortbio, :weburl
 
   def admin?
-    self.email == "zephram.cochrane.ncc.1701@gmail.com"
+    self.email == "zephram.cochrane.ncc.170!@gmail.com"
   end
  
   def guest?
