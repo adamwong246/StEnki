ae733909c4d368eaf406a817ec5d64c00da47ecf
adamwong246 <adam@spaceport.io>
2012-12-22 17:49:12 -0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,24 @@
 class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :remember_me
+  attr_accessible :email, :remember_me, :name
 
   has_many :comments
+  has_many :services
 
   def admin?
     self.email == "adamwong246@gmail.com@gmail.com" 
   end
 
   def identify
-    "guy incognito"
-
+    if self.name?
+      self.name
+    elsif self.email?
+      self.email
+    else
+      self.inspect
+    end
+    
   end
 
 end
