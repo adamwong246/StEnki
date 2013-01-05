023c9ba174a7cdce0c30b9a264104524d5953b8c
adamwong246 <adam@spaceport.io>
2013-01-05 13:02:36 -0800

    removed comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,35 +40,15 @@ module ApplicationHelper
 
   def my_menu_items
 
-    # require 'ruby-prof'
-
-    # Profile the code
-    # RubyProf.start
-
     Proc.new do |level_0|
 
       routes = Rails.application.routes.routes.select {|r| r.usable?}
-      # process_recursivly(routes, level_0)
-
-      # primary.item :books, 'Books', 'Books'
-      # primary.item :music, 'Music', 'Books'
-      # primary.item :dvds, 'Dvds', 'Books' do |dvd_level|
-      #   dvd_level.item :the_thing, "The Thing", "things" do |thing_level|
-      #     thing_level.item :wilford_symbol, "Wilford Brimley", "diabeetus"
-      #   end
-      # end
-
-      process_recursivly2(routes, level_0)
+      process_recursivly(routes, level_0)
     end
 
-    # result = RubyProf.stop
-
-    # # Print a flat profile to text
-    # printer = RubyProf::FlatPrinter.new(result)
-    # printer.print(STDOUT)
   end
 
-  def process_recursivly2(list, level)
+  def process_recursivly(list, level)
     list.each do |route|
 
       if !route.get_children.empty?
@@ -77,23 +57,6 @@ module ApplicationHelper
         end
       else
         level.item route.get_nav_item, route.get_label, route.get_url
-      end
-
-    end
-  end
-
-  def process_recursivly(list, level)
-
-    list.each do |elem|
-
-      children = elem.get_children
-
-      if children
-        level.item elem.get_nav_item, elem.get_label, elem.get_url do |child_level|
-          process_recursivly(children, child_level)
-        end
-      else
-        level.item elem.get_nav_item, elem.get_label, elem.get_url
       end
 
     end
