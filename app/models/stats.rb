055e35384f2afab5bed9915e6772fcaa740f8b57
adamwong246 <adam@spaceport.io>
2013-01-18 19:20:54 -0800

    old stuff

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -1,13 +1,13 @@
-class Stats
-  def post_count
-    Post.count
-  end
+# class Stats
+#   def post_count
+#     Post.count
+#   end
 
-  def comment_count
-    Comment.count
-  end
+#   def comment_count
+#     Comment.count
+#   end
 
-  def tag_count
-    Tag.count
-  end
-end
+#   def tag_count
+#     Tag.count
+#   end
+# end
