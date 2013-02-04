10387e38e5bf179a0f9fae5cce95d59c27037789
adamwong246 <adam@spaceport.io>
2013-02-04 15:30:48 -0800

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ StEnki::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :
+  config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'ifailedtheturingtest.herokuapp.com' } 
 
